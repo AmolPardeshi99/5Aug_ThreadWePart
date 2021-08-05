0f6a157f30dfc89b48269a25ac90fb8b9066955d
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-08-05 17:47:50 +0530

    Creating a working thread and performing sequential tasks in same thread one after another.

---
+++ app/src/main/java/com/example/threadwepart/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.threadwepart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button mBtnButton1, mBtnButton2;
+    WorkerThread workerThread;
+    private static final String TAG  = MainActivity.class.getName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        workerThread = new WorkerThread();
+        workerThread.start();
+        mBtnButton1 = findViewById(R.id.button1);
+        mBtnButton2 = findViewById(R.id.button2);
+        mBtnButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                workerThread.addTasktoMessageQueue(taskOne);
+            }
+        });
+
+        mBtnButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                workerThread.addTasktoMessageQueue(taskTwo);
+            }
+        });
+
+    }
+
+    private static Runnable taskOne = new Runnable() {
+        @Override
+        public void run() {
+            Log.d(TAG,"Task One "+Thread.currentThread().getName());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    private static Runnable taskTwo = new Runnable() {
+        @Override
+        public void run() {
+            Log.d(TAG,"Task One "+Thread.currentThread().getName());
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+}
